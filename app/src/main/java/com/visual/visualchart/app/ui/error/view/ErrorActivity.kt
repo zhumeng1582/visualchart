ab2c7e43494f67c4f5886660217925b22c7d282f
zhangchao <zhumeng1582@163.com>
2022-06-09 16:43:33 +0800

    mvvm

--- app/src/main/java/com/visual/visualchart/app/ui/error/view/ErrorActivity.kt
+++ app/src/main/java/com/visual/visualchart/app/ui/error/view/ErrorActivity.kt
@@ -1,15 +1,12 @@
 package com.visual.visualchart.app.ui.error.view
 
 import android.content.ClipData
-import android.content.Intent
 import android.graphics.drawable.ColorDrawable
-import android.net.Uri
 import android.os.Bundle
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash
 import com.blankj.utilcode.util.BarUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.visual.visualchart.app.base.BaseActivity
-import com.visual.visualchart.app.ext.showMessage
 import com.visual.visualchart.app.utils.SettingUtil
 import com.visual.visualchart.databinding.ActivityErrorBinding
 
@@ -38,20 +35,11 @@ class ErrorActivity : BaseActivity<BaseViewModel, ActivityErrorBinding>() {
         }
         mViewBind.errorSendError.clickNoRepeat {
             CustomActivityOnCrash.getStackTraceFromIntent(intent)?.let {
-                showMessage(it,"发现有Bug不去打作者脸？","必须打",{
-                    val mClipData = ClipData.newPlainText("errorLog",it)
-                    // 将ClipData内容放到系统剪贴板里。
-                    clipboardManager?.setPrimaryClip(mClipData)
-                    ToastUtils.showShort("已复制错误日志")
-                    try {
-                        val url = "mqqwpa://im/chat?chat_type=wpa&uin=824868922"
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                    } catch (e: Exception) {
-                        ToastUtils.showShort("请先安装QQ")
-                    }
-                },"我不敢")
+                val mClipData = ClipData.newPlainText("errorLog",it)
+                // 将ClipData内容放到系统剪贴板里。
+                clipboardManager?.setPrimaryClip(mClipData)
+                ToastUtils.showShort("已复制错误日志")
             }
-
 
         }
     }
